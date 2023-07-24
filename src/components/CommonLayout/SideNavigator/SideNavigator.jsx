ec6a43711eca2b275331be56368fcb3d462a82cf
sangwonme <paulmoguri@naver.com>
2023-07-25 01:04:30 +0900

    :sparkles: Add SideNavigator <Link> feature

--- src/components/CommonLayout/SideNavigator/SideNavigator.jsx
+++ src/components/CommonLayout/SideNavigator/SideNavigator.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+
 import styles from './SideNavigator.module.scss';
 
 import logoImg from '../../../assets/UI/logo.svg';
 import arrowImg from '../../../assets/UI/arrow.svg';
+import StyledLink from '../../StyledLink';
 
 const SideNavigator = () => {
   const navList = ['about', 'news', 'labs', 'publications', 'courses', 'community'];
@@ -10,13 +12,17 @@ const SideNavigator = () => {
   return ( <>
     <div className={styles.sideContainer}>
       <div className={styles.whiteBackground}>
-        <img src={logoImg} alt="logo" className={styles.logo}/>
+        <StyledLink to='/'>
+          <img src={logoImg} alt="logo" className={styles.logo}/>
+        </StyledLink>
         <div className={styles.navigatorContainer}>
           {navList.map((nav, i) =>
-            <div className={styles.navWrapper}>
-              <img src={arrowImg} alt="arrow" className={styles.arrow}/>
-              <p className={styles.navItem}>{nav.toUpperCase()}</p>
-            </div>
+            <StyledLink to={`/${nav}`}>
+              <div className={styles.navWrapper}>
+                <img src={arrowImg} alt="arrow" className={styles.arrow}/>
+                <p className={styles.navItem}>{nav.toUpperCase()}</p>
+              </div>
+            </StyledLink>
           )}
         </div>
       </div>
