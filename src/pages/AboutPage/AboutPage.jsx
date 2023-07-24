1afa656fd387f1cd5a61878740dc34411fc7521d
sangwonme <paulmoguri@naver.com>
2023-07-25 01:27:47 +0900

    :sparkles: Add <article> Layout & Style

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -12,6 +12,15 @@ const AboutPage = () => {
     <SideNavigator/>
     <main>
       <img src={bannerImg} alt="banner" />
+      <article>
+        <div className={styles.articleHeader}>
+          <h1 className={styles.headerTitle}>LABS</h1>
+          <p className={styles.headerDescription}>
+            We are the Human-Computer Interaction Group at Seoul National University, a multidisciplinary research consortium comprised of seven research labs from three distinct colleges.
+          </p>
+        </div>
+
+      </article>
     </main>
   </> );
 }
