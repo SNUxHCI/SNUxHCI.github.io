7a1b4aa9525081e36219e90e8c6bbd8ac29ec2e1
sangwonme <paulmoguri@naver.com>
2023-07-25 01:13:36 +0900

    :sparkles: Add BannerImage Layout & Style

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import styles from './AboutPage.module.scss';
+
+
+import bannerImg from '../../assets/UI/bannerImg.png';
+
+import SideNavigator from '../../components/CommonLayout/SideNavigator/SideNavigator';
+
 
 const AboutPage = () => {
   return ( <>
-    <p>AboutPage</p>
+    <SideNavigator/>
+    <main>
+      <img src={bannerImg} alt="banner" />
+    </main>
   </> );
 }
  
