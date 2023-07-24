4017b0252c33ffa758705de85d3db4ac1b610a4c
sangwonme <paulmoguri@naver.com>
2023-07-25 08:49:36 +0900

    :sparkles: Add bluearrow for current location in <SideNavigator>

--- src/components/CommonLayout/SideNavigator/SideNavigator.jsx
+++ src/components/CommonLayout/SideNavigator/SideNavigator.jsx
@@ -4,10 +4,14 @@ import styles from './SideNavigator.module.scss';
 
 import logoImg from '../../../assets/UI/logo.svg';
 import arrowImg from '../../../assets/UI/arrow.svg';
+import arrowBlueImg from '../../../assets/UI/arrow_blue.svg';
 import StyledLink from '../../StyledLink';
+import { useLocation } from 'react-router-dom';
 
 const SideNavigator = () => {
   const navList = ['about', 'news', 'labs', 'publications', 'courses', 'community'];
+
+  const currentLocation = useLocation().pathname.split("/")[1];;
 
   return ( <>
     <div className={styles.sideContainer}>
@@ -17,9 +21,9 @@ const SideNavigator = () => {
         </StyledLink>
         <div className={styles.navigatorContainer}>
           {navList.map((nav, i) =>
-            <StyledLink to={`/${nav}`}>
+            <StyledLink to={`/${nav}`} key={i}>
               <div className={styles.navWrapper}>
-                <img src={arrowImg} alt="arrow" className={styles.arrow}/>
+                <img src={nav==currentLocation? arrowBlueImg : arrowImg} alt="arrow" className={styles.arrow}/>
                 <p className={styles.navItem}>{nav.toUpperCase()}</p>
               </div>
             </StyledLink>
