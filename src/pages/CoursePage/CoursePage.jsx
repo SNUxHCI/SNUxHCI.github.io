5e89d5da904d14b442c0e8b034579841c960934e
sangwonme <paulmoguri@naver.com>
2023-07-25 09:42:27 +0900

    :lipstick: Add all corresponding module.scss file for pages

--- src/pages/CoursePage/CoursePage.jsx
+++ src/pages/CoursePage/CoursePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './CoursePage.module.scss';
 
 import SideNavigator from '../../components/CommonLayout/SideNavigator/SideNavigator';
 import MainLayout from '../../components/CommonLayout/MainLayout/MainLayout';
