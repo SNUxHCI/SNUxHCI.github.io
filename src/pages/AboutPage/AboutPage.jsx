bf7dfcd694ee90409cc6bf3ca7c7c923e05c4b93
sangwonme <paulmoguri@naver.com>
2023-07-27 13:44:54 +0900

    :sparkles: Add AboutPage Contents and styles

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -5,13 +5,22 @@ import SideNavigator from '../../components/CommonLayout/SideNavigator/SideNavig
 import MainLayout from '../../components/CommonLayout/MainLayout/MainLayout';
 import Header from '../../components/CommonLayout/Header/Header';
 
+import aboutImg from '../../assets/AboutImg/aboutImg.svg';
+
 
 const AboutPage = () => {
   return ( <>
    <Header/>
     <SideNavigator/>
     <MainLayout title='WHO WE ARE'>
-      {/* CONTENTS HERE */}
+      <div className={styles.container}>
+        <img src={aboutImg} alt="about" className={styles.aboutImg}/>
+        <p className={styles.description}>
+            We are the <b className={styles.bold}>Human-Computer Interaction Group </b>
+            at Seoul National University, a multidisciplinary research consortium
+            comprised of seven research labs from four distinct colleges.
+        </p>
+      </div>
     </MainLayout>
   </> );
 }
