59a20aa5f4c7a9e707f9dd3d00c42222d422fddd
sangwonme <paulmoguri@naver.com>
2023-07-28 14:31:42 +0900

    :sparkles: Add loading news data and thumbnails

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './NewsPage.module.scss';
+
+import { readYaml } from '../../functions/readYaml';
 
 import SideNavigator from '../../components/CommonLayout/SideNavigator/SideNavigator';
 import MainLayout from '../../components/CommonLayout/MainLayout/MainLayout';
 import Header from '../../components/CommonLayout/Header/Header';
+import NewsComponent from '../../components/NewsComponent/NewsComponent';
+
+const reformatDate = (date) => {
+  let dateParts = date.split('-');
+  return dateParts[2] + dateParts[0] + dateParts[1];
+}
+
+const getImgPathFromDate = (date) => {
+  const reformattedDate = reformatDate(date);
+  return `/images/news_${reformattedDate}.png`;
+}
 
 const NewsPage = () => {
+
+	const [selectedYear, setSelectedYear] = useState("All");
+	const [yearList, setYearList]   = useState([]);
+	const [news, setNews] 			    = useState([]);
+	const [filteredNews, setFilteredNews] = useState([]);
+
+  const fetchNews = async () => {
+    const newsRaw = await readYaml(require('../../assets/News/news.yaml'));
+		const news       = newsRaw.sort((a, b) => a.title.localeCompare(b.title)).sort((a, b) => b.year - a.year);
+		const yearList   = [...new Set(news.map(news => news.year))].sort((a, b) => b - a);
+		setNews(news);
+		setFilteredNews(news);
+		setYearList(["All", ...yearList.slice(0, 3), "Past"]);
+  }
+
+  useEffect(() => { fetchNews(); }, []);
+
+
   return ( <>
     <Header/>
     <SideNavigator/>
     <MainLayout title='NEWS'>
-      {/* CONTENTS HERE */}
+      {
+        filteredNews.map((news, i) =>
+        <NewsComponent key={i} title={news.title} date={news.date} thumbnail={getImgPathFromDate(news.date)}/>
+      )}
+
     </MainLayout>
   </> );
 }
