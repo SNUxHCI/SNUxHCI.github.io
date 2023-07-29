6dc58b9e0412839d3c420a42a7ba3dc06c9918f6
Hyeon Jeon <hj@hcil.snu.ac.kr>
2023-07-29 17:45:30 +0900

    implemented lab info view

--- src/pages/LabPage/Lab.jsx
+++ src/pages/LabPage/Lab.jsx
@@ -4,18 +4,21 @@ import styles from './Lab.module.scss';
 
 const Lab = (props) => {
 
-	const name = props.lab.name;
+	const name     = props.lab.name;
 	const fullname = props.lab.fullname;
-	const url = props.lab.url;
-	const prof = props.lab.prof;
+	const url      = props.lab.url;
+	const prof     = props.lab.prof;
 	const research = props.lab.research.split(",");
+	const logo     = props.lab.logo;
 
-	console.log(name, fullname, url, prof, research)
+	console.log(name, fullname, url, prof, research, logo)
 
 	return (
 		<div className={styles.labInfo}>
-			<div className={styles.labLogo}>
-
+			<div className={styles.labImgWrapper}>
+				{logo != undefined ? <img src={`./assets/Logo/${logo}.png`} className={styles.labImg}></img> :
+						<h4 className={styles.labImgFake}>{`${name} LAB`}</h4>
+				}
 			</div>
 			<div className={styles.labDesc}>
 				<h4 className={styles.labName}>{`${name} LAB`}</h4>
@@ -25,7 +28,7 @@ const Lab = (props) => {
 				<div className={styles.labResearchWrapper}>
 					{research.map((research, index) => {
 						return (
-							<p className={styles.labResearch} key={index}>{research}</p>
+							<div className={styles.labResearch} key={index}>{research}</div>
 						)
 					})}
 				</div>
